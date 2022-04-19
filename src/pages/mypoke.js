0f002f469bdb1eb3728c086445ae6f6fe1c41bfd
Bayu Aji <bayu.wicaksonojati@gmail.com>
2022-04-19 10:55:10 +0700

    added empty placeholder image mypokemon page

--- src/pages/mypoke.js
+++ src/pages/mypoke.js
@@ -4,6 +4,7 @@ import { css } from '@emotion/react';
 import React, { useContext, useEffect } from 'react';
 import PokeMyPokeItemList from '../components/PokeMyPokeItemList';
 import MyPokeContext from '../contexts/ContextMyPoke';
+import iconEmpty from '../assets/empty.png';
 
 const styleMyPokeHead = css({
     padding: '10px 25px',
@@ -24,6 +25,23 @@ const styleMyPokeBody = css({
     paddingBottom: '100px',
 })
 
+const styleEmpty = css({
+    marginTop: '150px',
+    textAlign: 'center',
+    img:{
+        maxWidth: '180px' 
+    },
+    p:{
+        color: 'gray',
+        marginTop: '3px',
+        fontSize: '.8em'
+    },
+    h4:{
+        color: 'gray',
+        marginBottom: '3px'
+    }
+})
+
 export default function MyPoke(){
     const {myPoke} = useContext(MyPokeContext);
 
@@ -38,9 +56,16 @@ export default function MyPoke(){
             </div>
             <div css={styleMyPokeBody}>
                 {
-                    myPoke.map( poke => (
-                        <PokeMyPokeItemList poke={poke} key={poke.nickname} />
-                    ))
+                    myPoke.length < 1 ?
+                        <div css={styleEmpty}>
+                            <img src={iconEmpty} alt=''/>
+                            <h4>You don't have Pokemons yet</h4>
+                            <p>Catch one in the Pokemon Detail page</p>
+                        </div> 
+                        :
+                        myPoke.map( poke => (
+                            <PokeMyPokeItemList poke={poke} key={poke.nickname} />
+                        ))
                 }
             </div>
         </React.Fragment>
